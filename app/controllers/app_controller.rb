8c68a15572293d9746e2a084a5c68b3bc217af38
Catherine Meyers <ccmeyers@gmail.com>
2014-07-19 21:28:12 -0400

    query using age params almost works

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -7,10 +7,15 @@ class AppController < Sinatra::Base
   end
 
   post '/show' do
-    raise params.inspect
+    years = params[:years]
+    months = params[:months]
+    @age = (years.to_i * 12 + months.to_i).to_s.to_sym
+    @location = params[:location]
+    @daytimes = params[:daytimes]
     #use params to query database
     #return lessons where params are true
       #where(:day => params[:daytimes])
+    @lessons = Lesson.where("min_age <= ?", @age)
     erb :'show'
   end
 
