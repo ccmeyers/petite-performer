26c50a106ce2512031fcb964a482ecc19cceb7ff
Catherine Meyers <ccmeyers@gmail.com>
2014-08-03 18:03:02 -0400

    tries to switch to postgres

--- Rakefile
+++ Rakefile
@@ -1,2 +1,9 @@
 require 'sinatra/activerecord/rake'
 require './config/environment'
+
+namespace :db do
+  task :seed do
+    seed_file = File.join('db/seeds.rb')
+    load(seed_file) if File.exist?(seed_file)
+  end
+end
--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,23 @@ Bundler.require
 
 #require 'nokogiri'
 #require 'open-uri'
+configure :production, :development do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
-# ActiveRecord::Base.establish_connection('postgres://localhost/my_little_musician')
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+  ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+  )
+end
+# ActiveRecord::Base.establish_connection('postgres://localhost/my-little-musician')
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
 
 Dir[File.join(File.dirname(__FILE__), "../app/controllers", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
