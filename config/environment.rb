da55464d668fdd0a7dbf6e76fdf0a289337bbd6f
Catherine Meyers <ccmeyers@gmail.com>
2014-08-12 10:37:16 -0400

    creates development environment that uses sqlite3

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Bundler.require
 configure :development do
   ActiveRecord::Base.establish_connection(
     :adapter => "sqlite3",
-    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+    :database => "db/development.sqlite3"
   )
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 20140804131332) do
 
   create_table "lessons", force: true do |t|
     t.string  "name"
